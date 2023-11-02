3df98122b78214351a9fb1b1b2084d0a8d93d8a2
ansel <anseltan.2022@scis.smu.edu.sg>
2023-11-02 16:05:46 +0800

    updated leaderboard

--- app/pages/leaderboard.php
+++ app/pages/leaderboard.php
@@ -28,100 +28,80 @@
                     <table>
                         <thead>
                             <tr>
-                                <th> Student Id <span class="icon-arrow">&UpArrow;</span></th>
-                                <th> Name <span class="icon-arrow">&UpArrow;</span></th>
-                                <th> Location <span class="icon-arrow">&UpArrow;</span></th>
-                                <th> Start Date <span class="icon-arrow">&UpArrow;</span></th>
-                                <th> Level <span class="icon-arrow">&UpArrow;</span></th>
-                                <th> Number of badges <span class="icon-arrow">&UpArrow;</span></th>
+                                <th> Student Id </th>
+                                <th> Name </th>
+                                <th> Level </th>
+                                <th> Number of Points </th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td> 1 </td>
-                                <td class="name"> <img src="../images/guy1.jpg" alt="">Alex Chan</td>
-                                <td> Seoul </td>
-                                <td> 17 Dec, 2022 </td>
+                                <td><strong> 1 </strong></td>
+                                <td> <img src="../images/guy1.jpg" alt="">Alex Chan</td>
                                 <td>
                                     <p class="status expert">Coding Expert</p>
                                 </td>
                                 <td> <strong> 100 </strong></td>
                             </tr>
                             <tr>
-                                <td> 2 </td>
-                                <td class="name"><img src="../images/guy2.jpg" alt="">Shawn Lim</td>
-                                <td> Kathmandu </td>
-                                <td> 27 Aug, 2023 </td>
+                                <td><strong> 2 </strong></td>
+                                <td><img src="../images/guy2.jpg" alt="">Shawn Lim</td>
                                 <td>
                                     <p class="status expert">Coding Expert</p>
                                 </td>
                                 <td> <strong>90</strong> </td>
                             </tr>
                             <tr>
-                                <td> 3</td>
-                                <td class="name"><img src="../images/girl1.jpg" alt=""> Chloe Tan </td>
-                                <td> Tokyo </td>
-                                <td> 14 Mar, 2023 </td>
+                                <td><strong> 3 </strong></td>
+                                <td><img src="../images/girl1.jpg" alt=""> Chloe Tan </td>
                                 <td>
                                     <p class="status whiz">Tech Whiz</p>
                                 </td>
                                 <td> <strong>80</strong> </td>
                             </tr>
                             <tr>
-                                <td> 4</td>
-                                <td class="name"><img src="../images/guy3.jpg" alt=""> Elson Chua </td>
-                                <td> New Delhi </td>
-                                <td> 25 May, 2023 </td>
+                                <td><strong> 4 </strong></td>
+                                <td><img src="../images/guy3.jpg" alt=""> Elson Chua </td>
                                 <td>
                                     <p class="status whiz">Tech Whiz</p>
                                 </td>
                                 <td> <strong>70</strong> </td>
                             </tr>
                             <tr>
-                                <td> 5</td>
-                                <td class="name"><img src="../images/girl2.png" alt=""> Rachel Lee </td>
-                                <td> Paris </td>
-                                <td> 23 Apr, 2023 </td>
+                                <td><strong> 5 </strong></td>
+                                <td><img src="../images/girl2.png" alt=""> Rachel Lee </td>
                                 <td>
                                     <p class="status proficient">Proficient Coder</p>
                                 </td>
                                 <td> <strong>60</strong> </td>
                             </tr>
                             <tr>
-                                <td> 6</td>
-                                <td class="name"><img src="../images/guy4.jpg" alt=""> Ali Abdul </td>
-                                <td> London </td>
-                                <td> 23 Apr, 2023 </td>
+                                <td><strong> 6 </strong></td>
+                                <td><img src="../images/guy4.jpg" alt=""> Ali Abdul </td>
                                 <td>
                                     <p class="status proficient">Proficient Coder</p>
                                 </td>
                                 <td> <strong>50</strong> </td>
                             </tr>
                             <tr>
-                                <td> 7</td>
-                                <td class="name"><img src="../images/guy5.jpg" alt=""> Fernando Alonso </td>
-                                <td> New York </td>
-                                <td> 20 May, 2023 </td>
+                                <td><strong> 7 </strong></td>
+                                <td><img src="../images/guy5.jpg" alt=""> Fernando Alonso </td>
                                 <td>
                                     <p class="status intermediate">Intermediate</p>
                                 </td>
                                 <td> <strong>40</strong> </td>
                             </tr>
                             <tr>
-                                <td> 8</td>
-                                <td class="name"><img src="../images/girl3.jpg" alt=""> Aayat Ali Khan </td>
-                                <td> Islamabad </td>
-                                <td> 30 Feb, 2023 </td>
+                                <td><strong> 8 </strong></td>
+                                <td><img src="../images/girl3.jpg" alt=""> Aayat Ali Khan </td>
                                 <td>
                                     <p class="status intermediate">Intermediate</p>
                                 </td>
                                 <td> <strong>30</strong> </td>
                             </tr>
                             <tr>
-                                <td> 9</td>
-                                <td class="name"><img src="../images/guy6.jpg" alt=""> Cody Chua </td>
-                                <td> Dhaka </td>
-                                <td> 22 Dec, 2023 </td>
+                                <td><strong> 9 </strong></td>
+                                <td><img src="../images/guy6.jpg" alt=""> Cody Chua </td>
                                 <td>
                                     <p class="status intermediate">Intermediate</p>
                                 </td>
@@ -134,10 +114,6 @@
         </div>
         
     </div>
-
-    <script>
-        // const leaderboard = Vue.
-    </script>
     
 </body>
 
