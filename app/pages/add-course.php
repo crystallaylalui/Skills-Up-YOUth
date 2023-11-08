c1477aa57d5dfa51a4163594b76710df33397d50
crystallaylalui <121093098+crystallaylalui@users.noreply.github.com>
2023-11-08 14:32:20 +0800

    Update add-course.php

--- app/pages/add-course.php
+++ app/pages/add-course.php
@@ -19,48 +19,10 @@
 
 <body>
     <div id="courses">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-black">
-            <div class="container-fluid">
-                <!-- Left Logo -->
-                <a class="navbar-brand" href="#" style="font-size: 30px; flex: 0;">
-                    <img src="../images/logo.jpg" alt="Logo" style="width: 150px;">
-                </a>
+    <div>
+        <?php include ('navbar.php'); ?>
+    </div>
 
-                <!-- Navbar Toggler Button -->
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <!-- Navbar Links -->
-                <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" style="font-size: 28px;">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" style="font-size: 28px;">Courses</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" style="font-size: 28px;">Jobs</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" style="font-size: 28px;">Leaderboard</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" style="font-size: 28px;">Profile</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <!-- Right Logo (Duplicated) -->
-                <a class="navbar-brand" href="#" style="font-size: 30px; flex: 0;">
-                    <img src="../images/filler.jpg" alt="Logo" width="50" height="50">
-                </a>
-            </div>
-            <div id="indicator"></div>
-        </nav>
 
         <h1>Courses</h1>
         <div class="container shadow-lg rounded-5 p-2" >
