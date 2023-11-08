3f4dca65c4f9da722e5774413b39e3ce38ae6507
clarisechua <clarisechuaen@gmail.com>
2023-11-08 18:48:09 +0800

    styling

--- server/model/ConnectionManager.php
+++ server/model/ConnectionManager.php
@@ -6,7 +6,7 @@ class ConnectionManager {
     $dbname = 'wad2_project';
     $username = 'root';
     $password = 'root';
-    $port ='8888';
+    $port ='3306';
     $url  = "mysql:host=$servername;dbname=$dbname;port=$port";
 
     return new PDO($url, $username, $password);
